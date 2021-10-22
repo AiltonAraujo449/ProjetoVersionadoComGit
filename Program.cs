02e278106ee93c97d7899698a8e48d2a50d24574
AiltonAraujo <ailton.corp@outlook.com>
2021-10-22 13:26:24 -0300

    Adicionada nova linha incluindo a versão da aplicação.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ namespace ProjetoVersionadoComGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
             Console.WriteLine(DateTime.Now.ToLongDateString());
+            
             Console.WriteLine("Versão da aplicação: 1.0");
         }
     }
